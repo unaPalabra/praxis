bce4fe275c9dbe9b517fb7e505f4c83f4630908b
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2023-02-09 14:01:13 +0700

    fp

--- src/Vebinar_OOP/Main.java
+++ src/Vebinar_OOP/Main.java
@@ -1,7 +1,29 @@
 package Vebinar_OOP;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Matrix matrix = new Matrix(2, 3);
 
+//        System.out.println(matrix.getH());
+//        System.out.println(matrix.getW());
+//        matrix.pprint();
+
+        Matrix m = new Matrix(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6}
+        });
+        m.pprint();
+        System.out.println(m.getH());
+        System.out.println(m.getW());
+
+        m.sum(10);
+        m.pprint();
+
+        matrix.sum(-5);
+        matrix.pprint();
+
+        m.sum(matrix);
     }
 }
---
+++ src/Vebinar_OOP/Matrix.java
@@ -0,0 +1,70 @@
+package Vebinar_OOP;
+
+public class Matrix {
+    private int[][] matrix;
+    private int w,h;
+
+    public Matrix(int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        this.matrix = new int[h][w];
+    }
+
+    public Matrix(int[][] matrix) {
+        this.h = matrix.length;
+        this.w = matrix[0].length;
+        this.matrix = matrix;
+    }
+
+    public void sum (int n){
+        for (int i = 0; i <h ; i++) {
+            for (int j = 0; j <w ; j++) {
+                this.matrix[i][j] += n;
+            }
+        }
+    }
+
+    public void sum(Matrix matrix){
+        if (this.h == matrix.getH() && this.w == matrix.getW()){
+
+        }else {
+            System.out.println("Матрицы разных размерностей, я не могу их сложить");
+        }
+
+    }
+    public void pprint(){
+        for (int i = 0; i < h ; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.printf(String.valueOf(this.matrix[i][j])+" \t");
+            }
+            System.out.println();
+        }
+    }
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+
+
+}
